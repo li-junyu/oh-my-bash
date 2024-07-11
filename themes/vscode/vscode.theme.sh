fbb5a3f68700a3fd7f240d0a0a32bdb0e20c1622
lijunyu <lijunyu@mycapital.net>
2024-07-11 21:39:14 +0800

    my themes

--- themes/vscode/vscode.theme.sh
+++ themes/vscode/vscode.theme.sh
@@ -18,7 +18,7 @@
 function _omb_theme_vscode_initialize {
     local userpart='`export XIT=$? \
         && [ ! -z "${GITHUB_USER}" ] && echo -n "\[\033[0;32m\]@${GITHUB_USER} " || echo -n "\[\033[0;32m\]\u " \
-        && [ "$XIT" -ne "0" ] && echo -n "\[\033[1;31m\]➜" || echo -n "\[\033[0m\]➜"`'
+        && [ "$XIT" -ne "0" ] && echo -n "\[\033[1;31m\]@" || echo -n "\[\033[0m\]@"`'
     local gitbranch='`\
         if [ "$(_omb_prompt_git config --get codespaces-theme.hide-status 2>/dev/null)" != 1 ]; then \
             export BRANCH=$(_omb_prompt_git symbolic-ref --short HEAD 2>/dev/null || _omb_prompt_git rev-parse --short HEAD 2>/dev/null); \
